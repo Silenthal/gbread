2c98964d74193e9a9cbe99a5707ca70e40059dbc
Silenthal <Silenthal@localhost>
2012-02-16 01:32:56 -0500

    Renamed and removed some things.

---
+++ Forms/LineSeparator.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GBRead
+{
+    public partial class LineSeparator : UserControl
+    {
+        public LineSeparator()
+        {
+            InitializeComponent();
+        }
+
+        private void LineSeparator_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pens.DarkGray, 0, 0, this.Width, 0);
+            e.Graphics.DrawLine(Pens.White, 0, 1, this.Width, 1);
+        }
+    }
+}
